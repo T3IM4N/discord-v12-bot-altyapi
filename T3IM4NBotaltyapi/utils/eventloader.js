22b10aee866c5a50471697520a0cab7e04c09f49
T3IM4N <58050280+T3IM4N@users.noreply.github.com>
2021-01-03 02:41:40 +0300

    Update eventloader.js

--- T3IM4NBotaltyapi/utils/eventloader.js
+++ T3IM4NBotaltyapi/utils/eventloader.js
@@ -1,10 +1,13 @@
 const reqEvent = (event) => require(`../events/${event}`);
-
+const ayarlar = require("../configs.json");
 module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
-  client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
-  client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
+  if(ayarlar.gircikLog = false){
+  } else {
+    client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
+    client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
+  }
   client.on('disconnect', reqEvent('disconnect'));
   client.on('reconnecting', reqEvent('reconnecting'));
   client.on('warn', reqEvent('warn'));
