8407488392f447228283fd4a5f07a8ed3d6202dc
T3IM4N <58050280+T3IM4N@users.noreply.github.com>
2021-02-26 19:09:10 +0300

    Delete T3IM4NBotaltyapi directory

--- T3IM4NBotaltyapi/events/error.js
+++
@@ -1,4 +0,0 @@
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-module.exports = e => {
-    console.log(chalk.bgRed(e.replace(regToken, 'Düzeltildi')));
-};
--- T3IM4NBotaltyapi/events/warn.js
+++
@@ -1,4 +0,0 @@
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-module.exports = e => {
-    console.log(chalk.bgYellow(e.replace(regToken, 'Düzeltildi')));
-};
