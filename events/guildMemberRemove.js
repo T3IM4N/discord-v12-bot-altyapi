06a25bdb8fbbd541af58d7247ed7708305326a2e
T3IM4N <58050280+T3IM4N@users.noreply.github.com>
2021-03-06 08:43:32 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js")
-const {prefix, version, embcolor, gircikKanal} = require('../configs.json');
+const conf = require('../configs.json');
 
 module.exports = async member => {
-  const cikis = await member.guild.channels.cache.get(gircikKanal);
+  const cikis = await member.guild.channels.cache.get(conf.gircikKanal);
 
   if (!cikis) return;
   cikis.send(new Discord.MessageEmbed()
-  .setColor(embcolor)
+  .setColor(conf.maincolor)
   .setAuthor(member.user.tag, member.user.avatarURL())
   .setThumbnail(member.user.avatarURL())
-  .setFooter(member.guild.name + " v" + version)
+  .setFooter(member.guild.name + " v" + conf.version)
   .setDescription("❌ | <@"+member.id+"> Sunucudan ayrıldı")
   .setTimestamp())
 };
