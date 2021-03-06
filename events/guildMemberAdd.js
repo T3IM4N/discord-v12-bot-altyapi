099f789674432905360d3aeefa6e6be0904533dc
T3IM4N <58050280+T3IM4N@users.noreply.github.com>
2021-03-06 08:44:13 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -25,7 +25,7 @@ module.exports = async member => {
     .setAuthor(member.user.tag, member.user.avatarURL())
     .setThumbnail(member.user.avatarURL())
     .addField('Hesap Oluşturma Tarihi:', `${moment(member.user.createdAt).format('DD')} ${aylar[moment(member.user.createdAt).format('MM')]} ${moment(member.user.createdAt).format('YYYY HH:mm:ss')}`)
-    .setFooter(member.guild.name)
+    .setFooter(client.user.username + " - v" +conf.version)
     .setDescription("✅ | <@"+member.id+"> Sunucuya katıldı")
     .setTimestamp())
 }
